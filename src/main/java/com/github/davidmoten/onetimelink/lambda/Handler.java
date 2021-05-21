0c76e3ef36514934fe5f860754946816c60ec0ee
Dave Moten <davidmoten@gmail.com>
2021-05-21 13:36:49 +1000

    dev

--- src/main/java/com/github/davidmoten/onetimelink/lambda/Handler.java
+++ src/main/java/com/github/davidmoten/onetimelink/lambda/Handler.java
@@ -1,14 +1,15 @@
 package com.github.davidmoten.onetimelink.lambda;
 
 import java.util.Map;
+import java.util.Optional;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Builder;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.github.davidmoten.aws.helper.BadRequestException;
 import com.github.davidmoten.aws.helper.ServerException;
+import com.github.davidmoten.aws.helper.StandardRequestBodyPassThrough;
 
 public final class Handler implements RequestHandler<Map<String, Object>, String> {
 
@@ -16,29 +17,35 @@ public final class Handler implements RequestHandler<Map<String, Object>, String
 
     @Override
     public String handleRequest(Map<String, Object> input, Context context) {
+        StandardRequestBodyPassThrough r = StandardRequestBodyPassThrough.from(input);
         try {
-            @SuppressWarnings("unchecked")
-            Map<String, Object> c = (Map<String, Object>) input.get("context");
-            String resourcePath = (String) c.get("resource-path");
+            String resourcePath = r.resourcePath().get();
             String dataBucketName = System.getenv("DATA_BUCKET_NAME");
             if (dataBucketName == null) {
                 throw new ServerException("environment variable DATA_BUCKET_NAME not set");
             }
             AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
-            @SuppressWarnings("unchecked")
-            Map<String, String> body = (Map<String, String>) input.get("body-json");
-            String key = body.get("key");
             if ("/store".equals(resourcePath)) {
+                @SuppressWarnings("unchecked")
+                Map<String, String> body = (Map<String, String>) input.get("body-json");
+                String key = body.get("key");
                 String value = body.get("value");
                 s3.putObject(dataBucketName, key, value);
                 return "stored";
             } else if ("/get".equals(resourcePath)) {
-                return s3.getObjectAsString(dataBucketName, key);
+                Optional<String> key = r.queryStringParameter("key");
+                if (!key.isPresent()) {
+                    throw new BadRequestException("key parameter not present");
+                } else {
+                    return s3.getObjectAsString(dataBucketName, key.get());
+                }
             } else {
                 throw new BadRequestException("unknown resource path: " + resourcePath);
             }
         } catch (IllegalArgumentException e) {
             throw new BadRequestException(e);
+        } catch (BadRequestException | ServerException e) {
+            throw e;
         } catch (Throwable e) {
             throw new ServerException(e);
         }
