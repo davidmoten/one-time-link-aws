035d226d62d4e8410717e7b78c97feff8f691ef7
Dave Moten <davidmoten@gmail.com>
2021-05-21 12:11:57 +1000

    dev

--- src/main/java/com/github/davidmoten/onetimelink/lambda/Handler.java
+++ src/main/java/com/github/davidmoten/onetimelink/lambda/Handler.java
@@ -4,28 +4,36 @@ import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Builder;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.github.davidmoten.aws.helper.BadRequestException;
 import com.github.davidmoten.aws.helper.ServerException;
 
 public final class Handler implements RequestHandler<Map<String, Object>, String> {
 
     private static String val = "not set";
-    
+
     @Override
     public String handleRequest(Map<String, Object> input, Context context) {
         try {
             @SuppressWarnings("unchecked")
             Map<String, Object> c = (Map<String, Object>) input.get("context");
             String resourcePath = (String) c.get("resource-path");
+            String dataBucketName = System.getenv("DATA_BUCKET_NAME");
+            if (dataBucketName == null) {
+                throw new ServerException("environment variable DATA_BUCKET_NAME not set");
+            }
+            AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+            @SuppressWarnings("unchecked")
+            Map<String, String> body = (Map<String, String>) input.get("body-json");
+            String key = body.get("key");
             if ("/store".equals(resourcePath)) {
-                @SuppressWarnings("unchecked")
-                Map<String, String> body = (Map<String, String>) input.get("body-json");
-                String key = body.get("key");
                 String value = body.get("value");
-                val = value;
+                s3.putObject(dataBucketName, key, value);
                 return "stored";
             } else if ("/get".equals(resourcePath)) {
-                return val;
+                return s3.getObjectAsString(dataBucketName, key);
             } else {
                 throw new BadRequestException("unknown resource path: " + resourcePath);
             }
