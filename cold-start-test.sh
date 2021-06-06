54c00ca03a01b23b881a8f32e8d2e9bd84b082b4
Dave Moten <davidmoten@gmail.com>
2021-06-06 11:47:10 +1000

    update sh

--- cold-start-test.sh
+++ cold-start-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 KEY=`tr -dc A-Za-z0-9 </dev/urandom | head -c 13` 
+LOG=/home/dave/cold-start-log.txt
 cd $WK/one-time-link-aws
 cp src/main/other/store.json /tmp/store.json
 sed -i "s/ZZKEYZZ/$KEY/g" /tmp/store.json
-date
+date >>$LOG
 for run in {1..10}; do
-  (time curl -X POST https://onetimelink.davidmoten.org/prod/store -H "Content-Type: application/json" --data-binary "@/tmp/store.json") 2>&1| grep real
+  (time curl -X POST https://onetimelink.davidmoten.org/prod/store -H "Content-Type: application/json" --data-binary "@/tmp/store.json") 2>&1| grep real >>$LOG
 done
 
