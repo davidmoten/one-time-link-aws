8577257e4d24e27bd24062a9f2247f5b56c003b8
David Moten <davidmoten@gmail.com>
2021-06-09 14:05:42 +1000

    fix imports

--- src/main/java/com/github/davidmoten/onetimelink/lambda/Handler.java
+++ src/main/java/com/github/davidmoten/onetimelink/lambda/Handler.java
@@ -25,7 +25,6 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.internal.handlers.EndpointAddressInterceptor;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
